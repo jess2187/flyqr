fc84c9ee29bb3ebdeb3a6a43aef1d9bb904b57e1
Will Ness <willness210@gmail.com>
2020-02-22 12:21:00 -0700

    setting up db connection

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import config
 
 app = Flask(__name__)
+mysql = MySQL(app)
 
 @app.route('/ping')
 def ping():
     return "pong"
+
+
+@app.route('/orgs')
+def orgs():
+    cur, conn = getDBConnection()
+    cur.execute("SELECT * FROM mysql.orgs")
+    rv = cur.fetchall()
+    return str(rv)
+
+def getDBConnection():
+    conn = mysql.connect(host=config.host,
+                        user = config.user,
+                        passwd = config.passwd,
+                        db = config.db)
+    return conn.cursor(), conn
+    
 
 if __name__ == '__main__':
     app.run()
