746ca0096daa214780f02937a2c671c71d7923de
Harsh Deshpande <hade7393@colorado.edu>
2020-02-22 20:02:04 -0700

    Started dummy values script

---
+++ backend/Database_Stuff/dummy_vals.sql
@@ -0,0 +1,7 @@
+use db_dev;
+
+insert into `Organization`(`id`,`name`,`email`,`Password`) values
+
+(`Walter Clements`,`firetruck@gmail.com`,`mostertruck`)
+(`Robert Downey Jr`,`stuff@colorado.edu`,`imstuff`)
+(`Joseph Joestar`,`joseph)
