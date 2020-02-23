922f83c3174d8676226b2c642b937b75de34386a
Evan Ram <evan@evanram.com>
2020-02-22 17:43:38 -0700

    wip: carousel for cards in dashboard

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { connect } from 'react-redux'
 import {
   SafeAreaView,
   View,
-  Text
+  Text,
+  Dimensions
 } from 'react-native'
+import Carousel from 'react-native-snap-carousel'
+
+const cards = []
+for (let i = 0; i < 100; i++) {
+  cards.push({})
+}
+
+const Card = ({ index }) => (
+  <View style={{margin:50, backgroundColor: 'red'}}><Text style={{fontSize: 20}}>I am a card #{index}</Text></View>
+)
+
+const { width: vpWidth, height: vpHeight } = Dimensions.get('window')
+
+const CardCarousel = (props) => {
+  const carouselRef = useRef(null)
+  const renderCard = Card
+
+  return (
+    <Carousel
+      ref={carouselRef}
+      data={cards}
+      renderItem={renderCard}
+      sliderWidth={vpWidth}
+      itemWidth={vpWidth}
+    />
+  )
+}
 
 const DashboardScreen = (props) => (
   <SafeAreaView>
     <Text>Dashboard screen..!</Text>
+    <CardCarousel />
   </SafeAreaView>
 )
 
