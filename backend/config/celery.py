b0e65db52c28e302c0f6224266d4cb3c364f624a
Evan Ram <evan@evanram.com>
2020-02-24 16:48:51 -0700

    Modify and section off gitignores

--- backend/config/celery.py
+++
@@ -1,6 +0,0 @@
-from celery import Celery
-from config import celeryconfig
-
-app = Celery('tasks.tasks', backend=celeryconfig.result_backend, broker=celeryconfig.broker_url)
-app.config_from_object(celeryconfig)
-import tasks.tasks
